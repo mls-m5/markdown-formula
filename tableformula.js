19991d281367d7150220a9660a61fef716be1467
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-04-09 21:21:53 +0200

    Orientation agnostic evaluation

--- tableformula.js
+++ tableformula.js
@@ -1,93 +1,108 @@
 "use strict";
 
-(function () {
+(function() {
+    let tables = document.getElementsByTagName("table");
+    let currentRow = 0;
 
-let tables = document.getElementsByTagName("table");
-let currentRow = 0;
+    let context = {};
 
-let conte;
-
-
-function getText(table, i, j) {
-    let col = table.rows[i].cells[j];
-    let text = col.innerText.trim();
-    return text;
-}
-
-// Convert to number if number
-function convert(text) {
-    if (isNaN(text)) {
+    function getText(table, i, j) {
+        let col = table.rows[i].cells[j];
+        let text = col.innerText.trim();
         return text;
     }
-    else {
-        return number(text);
-    }
-}
 
-function evaluateTable(table) {
-    let rowNames = {};
-    for (var i = 0; i < table.rows[0].cells.length; ++i) {
-        let name = table.rows[0].cells[i].innerText;
-        rowNames[name] = i;
-
-        let j = i; // To not capture changing value
-        window[name] = function(row) {
-            if (typeof row === "undefined") {
-                row = window.row;
-            }
-
-            return table.rows[row].cells[j].innerText;
+    // Convert to number if number
+    function convert(text) {
+        if (isNaN(text)) {
+            return text;
+        } else {
+            return number(text);
         }
     }
 
-    // Save previous text for autofill
-    let prevText = {};
-    let shouldAutoFill = {};
+    function evaluateCell(cell, i, j) {
+        if (!cell.hasOwnProperty("tempValue")) {
+            cell.tempValue = cell.innerText.trim();
+        }
 
-    for (let i = 0; i < table.rows.length; ++i) {
-        let row = table.rows[i];
-        currentRow = i;
-        window.row = i;
-        for (let j = 0, col; col = row.cells[j], j < row.cells.length; ++j) {
-            let text = col.innerText.trim();
-            if (text.length > 0 && text[0] === "=") {
-                let result = run(text.substring(1));
-                console.log("formula ", text.substring(1), " -> ", result);
-                col.innerText = result;
-                shouldAutoFill[j] = false;
+        if (typeof cell.tempValue === "string" && cell.tempValue.startsWith("=")) {
+            let old = cell.tempValue;
+            cell.innerText = cell.tempValue = run(cell.tempValue.substring(1));
+            if (typeof cell.tempValue === "undefined") {
+                throw "";
             }
-            else if (text === "...") {
-                shouldAutoFill[j] = true;
-            }
+        }
 
-            if (text === "" || text === "...") {
-                if (shouldAutoFill[j]) {
-                    text = prevText[j];
-                    if (text[0] === "=") {
-                        col.innerText = run(text.substring(1));
-                    }
-                    else {
-                        text = +text + 1;
-                        col.innerText = text;
+        return cell.innerText;
+    }
+
+
+    function evaluateTable(table) {
+        for (var i = 0; i < table.rows[0].cells.length; ++i) {
+            let name = table.rows[0].cells[i].innerText;
+
+            let j = i; // To not capture changing value
+            window[name] = function(row) {
+                if (typeof row === "undefined") {
+                    row = window.row;
+                }
+
+                return evaluateCell(table.rows[row].cells[j]);
+            }
+        }
+
+        // Save previous text for autofill
+        let prevText = {};
+        let shouldAutoFill = {};
+
+        for (let i = 0; i < table.rows.length; ++i) {
+            let row = table.rows[i];
+            currentRow = i;
+            window.row = i;
+            for (let j = 0, col; col = row.cells[j], j < row.cells.length; ++j) {
+                let text = col.innerText.trim();
+                if (text.length > 0 && text[0] === "=") {
+                    //let result = run(text.substring(1));
+                    //console.log("formula ", text.substring(1), " -> ", result);
+                    //col.innerText = result;
+                    shouldAutoFill[j] = false;
+                } else if (text === "...") {
+                    shouldAutoFill[j] = true;
+                }
+
+                if (text === "" || text === "...") {
+                    if (shouldAutoFill[j]) {
+                        text = prevText[j];
+                        if (text[0] === "=") {
+                            col.innerText = run(text.substring(1));
+                        } else {
+                            text = +text + 1;
+                            col.innerText = text;
+                        }
                     }
                 }
+                prevText[j] = text;
             }
-            prevText[j] = text;
+
+            for (let j = 0; j < row.cells.length; ++j) {
+                let cell = row.cells[j];
+                evaluateCell(cell, i, j);
+            }
         }
     }
-}
 
-function start() {
-    for (let i = 0; i < tables.length; ++i) {
-        evaluateTable(tables[i]);
+    function start() {
+        for (let i = 0; i < tables.length; ++i) {
+            evaluateTable(tables[i]);
+        }
     }
-}
 
-document.addEventListener('readystatechange', event => {
-    // When HTML/DOM elements are ready:
-    if (event.target.readyState === "interactive") {   //does same as:  ..addEventListener("DOMContentLoaded"..
-        start();
-    }
-});
+    document.addEventListener('readystatechange', event => {
+        // When HTML/DOM elements are ready:
+        if (event.target.readyState === "interactive") { //does same as:  ..addEventListener("DOMContentLoaded"..
+            start();
+        }
+    });
 
 })();
